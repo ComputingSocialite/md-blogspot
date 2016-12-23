aae79723e7d711e69c2637137efe7b8567537000
Kwantz <erickkwantantz123@gmail.com>
2016-12-23 15:59:03 +0700

    tambah js

--- js/ErickJS.js
+++ js/ErickJS.js
@@ -5,3 +5,7 @@ $('.blog-pager-older-link').addClass('btn');
 $('.blog-pager-newer-link').addClass('btn'); 
 $('.gsc-input').html( $('.gsc-input').html() + "<span class='glyphicon glyphicon-search'></span>");
 $('.foot.section > .widget.Attribution > .widget-content').html('Copyright © 2016 Computing Socialite');
+$('.popular-posts > ul > li').click( function() {
+	var link = $(this).find('a').attr("href");
+	window.location.href = link;
+}); 
