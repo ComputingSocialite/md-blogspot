45a8b20dc0298f0059162db5d062803b8811c123
Kwantz <erickkwantantz123@gmail.com>
2016-12-23 14:45:34 +0700

    tambah js dan css Erick

---
+++ js/ErickJS.js
@@ -0,0 +1,7 @@
+$('#blog-pager-older-link > a').html("<span>Next</span><span class='glyphicon glyphicon-chevron-right'></span>");
+$('#blog-pager-newer-link > a').html("<span class='glyphicon glyphicon-chevron-left'></span><span>Prev</span>");
+$('.home-link').addClass('btn');
+$('.blog-pager-older-link').addClass('btn');
+$('.blog-pager-newer-link').addClass('btn'); 
+$('.gsc-input').html( $('.gsc-input').html() + "<span class='glyphicon glyphicon-search'></span>");
+$('.foot.section > .widget.Attribution > .widget-content').html('Copyright © 2016 Computing Socialite');
